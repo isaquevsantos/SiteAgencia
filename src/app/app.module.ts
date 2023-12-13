c0608a2b65d1e4fe93eb13d9db5ffd517b2eb974
Isaque Santos <isaque.santos@ufms.br>
2023-12-13 15:17:09 -0400

    Reestruturei boa parte dos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/bak/header/header.component';
 import { FirtbannerComponent } from './components/firtbanner/firtbanner.component';
 import { ImportaciamktComponent } from './components/importaciamkt/importaciamkt.component';
-import { DepoimentosComponent } from './components/depoimentos/depoimentos.component';
 import { SobrenosComponent } from './components/sobrenos/sobrenos.component';
-import { FormularioComponent } from './components/formulario/formulario.component';
+import { FormularioComponent } from './components/bak/formulario/formulario.component';
 import { SliderComponent } from './components/slider/slider.component';
+import { ServicosComponent } from './components/servicos/servicos.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +17,10 @@ import { SliderComponent } from './components/slider/slider.component';
     HeaderComponent,
     FirtbannerComponent,
     ImportaciamktComponent,
-    DepoimentosComponent,
     SobrenosComponent,
     FormularioComponent,
-    SliderComponent
+    SliderComponent,
+    ServicosComponent
   ],
   imports: [
     BrowserModule,
